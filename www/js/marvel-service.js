9c0516f686b93e892f9c271b6b765b718281341c
Andre <andre.p23@hotmail.com>
2015-04-26 20:59:09 -0700

    Mejora de arquitectura (se agrego el js marvel-service)

---
+++ www/js/marvel-service.js
@@ -0,0 +1,65 @@
+(function(){
+
+    var app = angular.module('marvelService', []);
+
+
+marvelApp.service('apiMarvelConfigService', function() {
+    var self = this;
+    var timestamp = Date.now();
+    var public_key = "0f3ada3d3a88add4430fa9b49de33558";
+    var private_key = "8e65faa79eaa382e9a6b215cb545b5ec06d4afb5";
+    this.marvelUrlAPI = "http://gateway.marvel.com/v1/public";
+    this.marvelTimeStamp = timestamp;
+    this.marvelPublicKey = public_key;
+    this.marvelPrivateKey = private_key;
+    this.marvelHash = md5(timestamp.toString() + private_key + public_key);
+});
+
+
+
+marvelApp.service('apiMarvelCharactersService', ['$http', '$log', 'apiMarvelConfigService', function($http, $log, apiMarvelConfigService) {
+    this.topTenCharacters;
+    var self = this;
+    this.getTopTenCharacters = function() {
+        var data = {
+            ts: apiMarvelConfigService.marvelTimeStamp,
+            apikey: apiMarvelConfigService.marvelPublicKey,
+            hash: apiMarvelConfigService.marvelHash,
+            limit: "10"
+        };
+
+        var params = "?" + $.param(data);
+
+        return ($http.get(apiMarvelConfigService.marvelUrlAPI + '/characters' + params).then(handleSuccess, handleError));
+    };
+
+    this.getCharacter=function(marvelSuperHeroeId) {
+        var data = {
+            ts: apiMarvelConfigService.marvelTimeStamp,
+            apikey: apiMarvelConfigService.marvelPublicKey,
+            hash: apiMarvelConfigService.marvelHash,
+            limit: "1"
+        };
+
+        var params = "?" + $.param(data);
+
+        return ($http.get(apiMarvelConfigService.marvelUrlAPI + '/characters/'+marvelSuperHeroeId + params).then(handleSuccess, handleError));
+    };
+
+    function handleError(response) {
+        if (!angular.isObject(response.data) ||
+            !response.data.message
+        ) {
+            return ($q.reject("An unknown error occurred."));
+        }
+        return ($q.reject(response.data.message));
+
+    }
+
+    function handleSuccess(response) {
+        $log.info(response);
+        return (response.data.data.results);
+    }
+
+}]);
+  })();
